3d9156c8cfc9d55e0ebac70c4e1829b8234d7094
Benjamin Silas Rhodes <benjamin.s.rhodes@gmail.com>
2017-04-30 07:13:35 -0700

    fix test

--- test/dq_test.exs
+++ test/dq_test.exs
@@ -2,7 +2,7 @@ defmodule DQTest do
   use ExUnit.Case
 
   test "new_id" do
-    assert String.starts_with?(DQ.new_id, "que")
+    assert String.starts_with?(DQ.new_id, "DQ")
     assert DQ.new_id != DQ.new_id
   end
 end
