4da02623a07b15065749358c9326cbaee95583bb
Benjamin Silas Rhodes <benjamin.s.rhodes@gmail.com>
2018-04-01 12:52:25 -0700

    update pop query, the lock method was starving other consumers

--- lib/dq/adapters/ecto.ex
+++ lib/dq/adapters/ecto.ex
@@ -10,20 +10,21 @@ defmodule DQ.Adapters.Ecto do
   alias Ecto.Adapters.SQL
 
   def info(queue) do
-    %Postgrex.Result{columns: columns, rows: [row]} = sql(queue, Statements.info, [])
-    cols = Enum.map(columns, &(String.to_atom(&1)))
+    %Postgrex.Result{columns: columns, rows: [row]} = sql(queue, Statements.info(), [])
+    cols = Enum.map(columns, &String.to_atom(&1))
     {:ok, struct(Info, Enum.zip(cols, row))}
   end
 
   def push(queue, jobs) when is_list(jobs) do
-    Enum.each(jobs, fn({module, args}) ->
+    Enum.each(jobs, fn {module, args} ->
       queue.push(module, args)
     end)
   end
 
   def sql(queue, statement, args) do
     repo = queue.config[:repo]
-    table = queue.config |> Keyword.get(:table, :jobs) |> Atom.to_string
+    table = queue.config |> Keyword.get(:table, :jobs) |> Atom.to_string()
+
     statement =
       statement
       |> String.replace("$TABLE$", table)
@@ -48,7 +49,7 @@ defmodule DQ.Adapters.Ecto do
 
     max_runtime_seconds = Keyword.get(opts, :max_runtime_seconds, 30)
     payload = Encoder.encode({module, args})
-    results = sql(queue, Statements.insert, [payload, max_runtime_seconds, scheduled_at])
+    results = sql(queue, Statements.insert(), [payload, max_runtime_seconds, scheduled_at])
     %Postgrex.Result{columns: ["id"], rows: [[job_id]]} = results
     {:ok, job_id}
   end
@@ -63,32 +64,35 @@ defmodule DQ.Adapters.Ecto do
 
   defp cast_scheduled_at(scheduled_at), do: scheduled_at
 
-  def pop(queue, _) do
-    res  = sql(queue, Statements.pop, [])
+  def pop(queue, limit) do
+    res = sql(queue, Statements.pop(limit), [])
     jobs = decode_results(res, queue)
     {:ok, jobs}
   end
 
   def ack(queue, job) do
-    %Postgrex.Result{num_rows: 1} = sql(queue, Statements.ack, [job.id])
+    %Postgrex.Result{num_rows: 1} = sql(queue, Statements.ack(), [job.id])
     :ok
   end
 
   def nack(queue, job, message) do
-    retries  = job.error_count
+    retries = job.error_count
     interval = queue.config[:retry_intervals] |> Enum.at(retries)
+
     if interval do
-      sql(queue, Statements.nack, [message, "#{interval}", job.id])
+      sql(queue, Statements.nack(), [message, "#{interval}", job.id])
     else
-      sql(queue, Statements.nack_dead, [message, job.id])
+      sql(queue, Statements.nack_dead(), [message, job.id])
     end
+
     :ok
   end
 
   defp decode_results(%Postgrex.Result{columns: columns, rows: rows}, queue) do
     type = queue.job_struct()
-    cols = Enum.map(columns, &(String.to_atom(&1)))
-    Enum.map(rows, fn(row) ->
+    cols = Enum.map(columns, &String.to_atom(&1))
+
+    Enum.map(rows, fn row ->
       job = struct(type, Enum.zip(cols, row))
       {module, args} = Encoder.decode(job.payload)
       %{job | module: module, args: args}
@@ -97,51 +101,51 @@ defmodule DQ.Adapters.Ecto do
 
   def dead(queue, limit \\ 100) do
     jobs =
-      sql(queue, Statements.dead, [limit])
+      sql(queue, Statements.dead(), [limit])
       |> decode_results(queue)
 
     {:ok, jobs}
   end
 
   def dead_ack(queue, %{id: id}) when is_integer(id) do
-    %Postgrex.Result{num_rows: 1} = sql(queue, Statements.ack, [id])
+    %Postgrex.Result{num_rows: 1} = sql(queue, Statements.ack(), [id])
     :ok
   end
 
   def dead_ack(queue, %{id: id} = job) when is_binary(id) do
-    job = %{job | id: id |> String.to_integer}
+    job = %{job | id: id |> String.to_integer()}
     dead_ack(queue, job)
   end
 
   def dead_retry(queue, %{id: id}) when is_integer(id) do
-    %Postgrex.Result{num_rows: 1} = sql(queue, Statements.retry, [id])
+    %Postgrex.Result{num_rows: 1} = sql(queue, Statements.retry(), [id])
     :ok
   end
 
   def dead_retry(queue, %{id: id} = job) when is_binary(id) do
-    job = %{job | id: id |> String.to_integer}
+    job = %{job | id: id |> String.to_integer()}
     dead_retry(queue, job)
   end
 
   def dead_purge(queue) do
-    sql(queue, Statements.dead_purge, [])
+    sql(queue, Statements.dead_purge(), [])
     :ok
   end
 
   def purge(queue) do
-    sql(queue, Statements.purge, [])
+    sql(queue, Statements.purge(), [])
     :ok
   end
 
   def encode(job) do
     job
-    |> Encoder.encode
-    |> Base.encode64
+    |> Encoder.encode()
+    |> Base.encode64()
   end
 
   def decode(job) do
     job
-    |> Base.decode64!
-    |> Encoder.decode
+    |> Base.decode64!()
+    |> Encoder.decode()
   end
 end
--- lib/dq/adapters/ecto/statements.ex
+++ lib/dq/adapters/ecto/statements.ex
@@ -8,73 +8,79 @@ defmodule DQ.Adapters.Ecto.Statements do
   end
 
   def nack do
-  """
-UPDATE $TABLE$ SET
-  error_count = error_count + 1,
-  error_message = $1,
-  status = 'pending',
-  scheduled_at = now() at time zone 'UTC' + ($2::text || ' ' || 'seconds'::text)::interval,
-  dequeued_at = NULL,
-  deadline_at = NULL
-WHERE
-  id = $3
+    """
+    UPDATE $TABLE$ SET
+    error_count = error_count + 1,
+    error_message = $1,
+    status = 'pending',
+    scheduled_at = now() at time zone 'UTC' + ($2::text || ' ' || 'seconds'::text)::interval,
+    dequeued_at = NULL,
+    deadline_at = NULL
+    WHERE
+    id = $3
     """
   end
 
   def nack_dead do
-  """
-UPDATE $TABLE$ SET
-  error_count = error_count + 1,
-  error_message = $1,
-  status = 'dead',
-  dequeued_at = NULL,
-  deadline_at = NULL
-WHERE
-  id = $2
+    """
+    UPDATE $TABLE$ SET
+    error_count = error_count + 1,
+    error_message = $1,
+    status = 'dead',
+    dequeued_at = NULL,
+    deadline_at = NULL
+    WHERE
+    id = $2
     """
   end
 
-  def pop do
+  def pop(limit) do
     """
-WITH candidates AS (
-   SELECT id, payload,
-      scheduled_at as scheduled_at
-   FROM $TABLE$
-   WHERE
-     dequeued_at IS NULL
-     AND
-     status = 'pending'
-   ORDER BY scheduled_at nulls first
-   LIMIT 1
- ),  selected AS (
-   SELECT id FROM candidates
-   WHERE
-      scheduled_at <= now() at time zone 'utc' OR scheduled_at is NULL
+    with cte as (
+      SELECT id FROM $TABLE$
+      WHERE
+        dequeued_at IS NULL
       AND
-      pg_try_advisory_xact_lock(id)
-   ORDER BY scheduled_at nulls first
-   LIMIT 1
-)
-UPDATE $TABLE$ SET
-  status = 'running',
-  dequeued_at = now() at time zone 'utc',
-  deadline_at = now() at time zone 'utc' + ($TABLE$.max_runtime_seconds::text || ' ' || 'seconds'::text)::interval
-WHERE id = (SELECT id FROM selected)
-AND dequeued_at IS NULL
-RETURNING *
- """
+        status = 'pending'
+      AND
+        scheduled_at <= now() at time zone 'utc' OR scheduled_at is NULL
+      AND
+        dequeued_at IS NULL
+      LIMIT #{limit}
+      FOR UPDATE SKIP LOCKED
+    )
+    update $TABLE$ set
+     status = 'running',
+     dequeued_at = now() at time zone 'utc',
+     deadline_at = now() at time zone 'utc' + ($TABLE$.max_runtime_seconds::text || ' ' || 'seconds'::text)::interval
+    FROM cte
+    WHERE cte.id = $TABLE$.id
+    RETURNING *
+    """
+  end
+
+  def reserve(ids) do
+    """
+    UPDATE $TABLE$ SET
+     status = 'running',
+     dequeued_at = now() at time zone 'utc',
+     deadline_at = now() at time zone 'utc' + ($TABLE$.max_runtime_seconds::text || ' ' || 'seconds'::text)::interval
+    WHERE id in (#{ids})
+    AND dequeued_at IS NULL
+    RETURNING *
+    """
   end
 
   def retry do
-  """
-UPDATE $TABLE$ SET
-  error_count = 0,
-  error_message = NULL,
-  status = 'pending',
-  dequeued_at = NULL,
-  deadline_at = NULL
-WHERE
-  id = $1
+    """
+    UPDATE $TABLE$ SET
+    error_count = 0,
+    error_message = NULL,
+    status = 'pending',
+    dequeued_at = NULL,
+    deadline_at = NULL
+    WHERE
+    id = $1
     """
   end
 
@@ -92,11 +98,11 @@ WHERE
 
   def info do
     """
-select
-  (select count(*) from $TABLE$ where status = 'pending' AND scheduled_at IS NULL) as pending,
-  (select count(*) from $TABLE$ where status = 'pending' AND scheduled_at IS NOT NULL) as delayed,
-  (select count(*) from $TABLE$ where status = 'running') as running,
-  (select count(*) from $TABLE$ where status = 'dead') as dead;
+    select
+    (select count(*) from $TABLE$ where status = 'pending' AND scheduled_at IS NULL) as pending,
+    (select count(*) from $TABLE$ where status = 'pending' AND scheduled_at IS NOT NULL) as delayed,
+    (select count(*) from $TABLE$ where status = 'running') as running,
+    (select count(*) from $TABLE$ where status = 'dead') as dead;
     """
   end
 end
