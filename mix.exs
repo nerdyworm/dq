a69b3230fbd557e43ff1464dcb8e9b9bf1dfcc75
ben <ben@nerdyworm.com>
2018-08-09 20:35:12 -0700

    basic cloud pub sub

--- mix.exs
+++ mix.exs
@@ -2,13 +2,15 @@ defmodule DQ.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :dq,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     elixirc_paths: elixirc_paths(Mix.env),
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :dq,
+      version: "0.1.0",
+      elixir: "~> 1.4",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
   end
 
   def application do
@@ -16,7 +18,7 @@ defmodule DQ.Mixfile do
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support", "test/adapters"]
-  defp elixirc_paths(_),     do: ["lib"]
+  defp elixirc_paths(_), do: ["lib"]
 
   defp deps do
     [
@@ -36,6 +38,10 @@ defmodule DQ.Mixfile do
       {:poison, "~> 3.0"},
       {:hackney, "~> 1.9"},
       {:sweet_xml, ">= 0.5.0"},
+
+      # Google Cloud Pubsub
+      {:diplomat, "~> 0.2"},
+      {:google_api_pub_sub, "~> 0.0.1"}
     ]
   end
 end
